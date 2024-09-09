7efa44a9c90be1c6e592b2852b6df7545a755f6e
GameDevVaibhav <gamedevvaibhav@gmail.com>
2024-09-09 10:40:47 +0530

    Added vehicles

--- Assets/Scripts/SwipeButton.cs
+++ Assets/Scripts/SwipeButton.cs
@@ -4,20 +4,11 @@ using DG.Tweening;
 
 public class SwipeButton : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    public RectTransform parentContainer;  // The parent container (UI element) to stretch
-    public float stretchDuration = 0.3f;  // Duration for the stretch effect
-    public float squashFactor = 0.8f;    // Factor to squash the container
-    public float stretchFactor = 1.2f;    // Factor to stretch the container
-    public int bounceCount = 3;          // Number of bounce iterations
+    public RectTransform parentContainer;  // The parent container (UI element) to move
+    public float bounceDuration = 0.5f;    // Duration of the bounce effect
+    public float elasticity = 0.5f;        // Elasticity factor for the bounce effect (higher = more elastic)
 
-    private Vector2 originalSize; // To store the original size of the container
-    private float dragEndPositionX; // To store the position of X when drag ends
-
-    private void Start()
-    {
-        // Store the original size of the container
-        originalSize = parentContainer.sizeDelta;
-    }
+    private float dragEndPositionX;         // To store the position of X when drag ends
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -37,41 +28,23 @@ public class SwipeButton : MonoBehaviour, IDragHandler, IEndDragHandler
 
         if (position.x < 79 && position.x >= -79)
         {
-            transform.localPosition = new Vector3(-79, position.y, position.z);
+            BounceBackToLeftWall();
         }
         else
         {
             GameManager.Instance.OnPlayButtonClicked();
-            transform.localPosition = new Vector3(-79, position.y, position.z);
-        }
-
-        // Check if the user tried to drag beyond the limit and apply stretch and squash effect
-        if (dragEndPositionX > 79 || dragEndPositionX < -79)
-        {
-            ApplyStretchAndSquash();
+            BounceBackToLeftWall();
         }
     }
 
-    private void ApplyStretchAndSquash()
+    private void BounceBackToLeftWall()
     {
-       
+        // Kill any existing animations on the button
+        transform.DOKill();
 
-        // Start the bounce sequence
-        Sequence bounceSequence = DOTween.Sequence();
-        
-
-        // Repeat the bounce sequence with decreasing strength
-        for (int i = 1; i < bounceCount; i++)
-        {
-            float strengthMultiplier = Mathf.Pow(0.5f, i); // Decrease strength gradually
-            bounceSequence.Append(parentContainer.DOSizeDelta(new Vector2(originalSize.x * (1 + strengthMultiplier), originalSize.y * (1 - strengthMultiplier)), stretchDuration / 2)
-                .SetEase(Ease.OutQuad))
-                .Append(parentContainer.DOSizeDelta(new Vector2(originalSize.x * (1 - strengthMultiplier), originalSize.y * (1 + strengthMultiplier)), stretchDuration / 2)
-                .SetEase(Ease.InQuad));
-        }
-
-        // Return to original size at the end
-        bounceSequence.Append(parentContainer.DOSizeDelta(originalSize, stretchDuration / 2)
-            .SetEase(Ease.OutQuad));
+        // Apply elastic bounce-back effect
+        transform.DOLocalMoveX(-79, bounceDuration)
+            .SetEase(Ease.OutBounce, elasticity)  // Use OutElastic easing for bounce
+            .OnComplete(() => transform.localPosition = new Vector3(-79, transform.localPosition.y, transform.localPosition.z));
     }
 }
